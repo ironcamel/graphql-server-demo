7ffbad4069324c4bf226c69bf77960eaee39f116
Naveed Massjouni <naveed@vt.edu>
2026-06-20 19:42:10 -0400

    initial commit

---
+++ src/index.js
@@ -0,0 +1,61 @@
+import cors from "cors";
+import express from "express";
+import { ApolloServer, gql } from "apollo-server-express";
+
+const app = express();
+
+app.use(cors());
+
+const schema = gql`
+  type Query {
+    users: [User!]
+    me: User
+    user(id: ID!): User
+  }
+
+  type User {
+    id: ID!
+    username: String!
+  }
+`;
+
+let users = {
+  1: {
+    id: "1",
+    username: "Naveed03",
+  },
+  2: {
+    id: "2",
+    username: "Dave Davids",
+  },
+};
+
+const resolvers = {
+  Query: {
+    users: () => {
+      return Object.values(users);
+    },
+    user: (parent, { id }) => {
+      return users[id];
+    },
+    me: (parent, args, { me }) => {
+      return me;
+    },
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs: schema,
+  resolvers,
+  context: {
+    me: users[1],
+  },
+});
+
+await server.start();
+
+server.applyMiddleware({ app, path: "/graphql" });
+
+app.listen({ port: 8000 }, () => {
+  console.log("Apollo Server on http://localhost:8000/graphql");
+});
