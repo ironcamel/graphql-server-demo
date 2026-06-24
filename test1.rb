ed82232b31ea5a706b417d47139de93802d91b6e
Naveed Massjouni <naveed@vt.edu>
2026-06-23 22:33:12 -0400

    add test script

---
+++ test1.rb
@@ -0,0 +1,31 @@
+#!/bin/env ruby
+require 'json'
+require 'net/http'
+
+query1 = %q{
+  {
+    messages(limit: 100) {
+      edges {
+        text
+        id
+      }
+      pageInfo {
+        endCursor
+        hasNextPage
+      }
+    }
+  }
+}
+
+query2 = %q{
+  mutation {
+    createMessage(text: "allo") {
+      id
+    }
+  }
+}
+
+json = JSON.generate({ query: query1 })
+uri = URI('http://localhost:8000/graphql')
+response = Net::HTTP.post(uri, json, "Content-Type" => "application/json")
+puts JSON.pretty_generate(JSON.parse(response.body))
